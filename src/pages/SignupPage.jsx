8d551a2e1fb99c187ce26b3828130e7cf9903459
anikemide0902-rgbecho <anikemide0902@gmail.com>
2026-03-02 11:25:17 -0500

    signup page added

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
 import "./SignupPage.css"
+import NavComponent from '../components/NavComponent'
+
+
 const SignupPage = () => {
   return (
-    <div>SignupPage</div>
+    <div className='sign-up'>
+
+      <NavComponent />
+      <div className='all'>
+        <div className='up'>
+          <h2>Create Account</h2>
+          <p>Please sign up to book appointment</p>
+        </div>
+
+        <div className='form'>
+          <div className='name'>
+            <label htmlFor="fullname">Full Name</label>
+            <input type="text" name='fullname'/>
+          </div>
+
+          <div className='name'>
+            <label htmlFor="email">Email</label>
+            <input type="email" name='email'/>
+          </div>
+
+          <div className='name'>
+            <label htmlFor="password">Password</label>
+            <input type="password" name='password'/>
+          </div>
+
+          <button className='button-blue'>Create Account</button>
+        </div>
+
+        <div className='down'>
+          <p>Already have an account? <a href="">Login here</a></p>
+        </div>
+      </div>
+    </div>
   )
 }
 
